4a8d9ac0fa131d035a641b3cfc96ba86b2446685
gabkoi21 <12345678+username@users.noreply.github.com>
2024-11-05 23:23:42 +0000

    Add index to root directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
-{
-  import("tailwindcss").Config;
-}
+// {
+//   import("tailwindcss").Config;
+// }
+// module.exports = {
+//   content: ["/index.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("tailwindcss-elevation")],
+// };
+
 module.exports = {
-  content: ["./public/index.html"],
+  content: [
+    "./index.html", // Include the root index.html file
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Include other files in src folder if needed
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-elevation")],
+  plugins: [],
 };
